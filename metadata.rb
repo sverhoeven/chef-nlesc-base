406eb6ff2a583628226f3e5d8909cb90e1edbf9e
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2013-09-23 17:54:43 +0200

    Add ntp, openssh and java as recipes that must be executed on all machines.

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,7 @@ description      'Installs/Configures nlesc-base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "openssh"
+depends "ntp"
+depends "java"
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+node.default['ntp']['servers'] = ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org"]
+node.default['java']['jdk_version'] = 7
+
+include_recipe "ntp"
+include_recipe "openssh"
+include_recipe "java"
