83c0524f8e2fc0dd229d0febb072e23820c8f897
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2013-09-25 14:58:30 +0200

    Install both  OpenJDK and Oracle JDK (default).

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,13 +3,13 @@
 # Recipe:: default
 #
 # Copyright (C) 2013 Netherlands eScience Center
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,12 +18,14 @@
 #
 
 node.default['ntp']['servers'] = ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org"]
-node.default['java']['jdk_version'] = 7
+node.override['java']['jdk_version'] = 7
 node.default['java']['oracle']['accept_oracle_download_terms'] = true
+# Without specifying packages openjdk 6 was being installed
+node.default['java']['openjdk_packages'] = ["openjdk-7-jdk", "openjdk-7-jre-headless"]
 
 include_recipe "ntp"
 include_recipe "openssh"
 include_recipe "git"
+# Install 2 jdks with the oracle jdk as default
+include_recipe "java::openjdk"
 include_recipe "java::oracle"
-# TODO see if 2 jdk's can be installed and the oracle jdk as default
-#include_recipe "java::openjdk"
